7cde6f9d1aa19ce62a3b6532577191f76fade48b
dluman <dluman@allegheny.edu>
2023-03-02 14:51:12 -0500

    Adding TODOs

--- contracts/test.sol
+++ contracts/test.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.16 <0.9.0;
 
+// TODO: Keep in mind concepts around "mutual aid societies"
+
 contract SimpleStorage {
     uint betAmt;
     uint memberLimit = 100;
     address[] members = new address[](memberLimit);
-
 
     function setBet(uint x) public {
         betAmt = x;
@@ -16,6 +17,8 @@ contract SimpleStorage {
     }
 
     function storeMembers(address member) public{
+        // TODO: Store contributor by address
+        // TODO: Associate the wallet address with a given Tx <--- mapping?
         members.push() = member;
     }
 }
